15176882dc37b1bb7e9e15e3a0b099fc031d257f
yurii.sytnyk@spd-ukraine.com <yurii.sytnyk@spd-ukraine.com>
2025-01-17 15:25:41 +0200

    fix: updated once.test.js

--- exercises/src/once/once.test.js
+++ exercises/src/once/once.test.js
@@ -6,14 +6,14 @@ describe("once", () => {
   it("should return 6 the first time and undefined the second time", () => {
     const onceFn = once((a, b, c) => a + b + c);
 
-    expect(onceFn([1, 2, 3])).toBe(6);
-    expect(onceFn([2, 3, 4])).toBeUndefined();
+    expect(onceFn(1, 2, 3)).toBe(6);
+    expect(onceFn(2, 3, 4)).toBeUndefined();
   });
 
   it("should return 140 the first time and undefined the second time", () => {
     const onceFn = once((a, b, c) => a * b * c);
 
-    expect(onceFn([5, 7, 4])).toBe(140);
-    expect(onceFn([2, 3, 4])).toBeUndefined();
+    expect(onceFn(5, 7, 4)).toBe(140);
+    expect(onceFn(2, 3, 4)).toBeUndefined();
   });
 });
